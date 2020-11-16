fff8c9001806a886bc137d2a9c33f7f44dd3235a
jacob <jbrooker@hawk.iit.edu>
2020-11-16 15:05:55 -0600

    Problem 3

--- pp-04/site.js
+++ pp-04/site.js
@@ -26,24 +26,41 @@ console.log('The value of x is', x, '-- it should be 5.');
   can accurately test your work.
 */
 
-
-(function math() {
- var x = 5;
- function double(num) {
-   var x = num * 2;
-   return x;
+/*
+function math() {
+  var x = 5;
+  function double(num) {
+    var x = num * 2;
+    return x;
   }
- }
 double(6);
 console.log('The value of x is', x, '-- it should be 5.');
 }
 math();
-
-
+*/
 
 
 /*
   Step 3. Rewrite your corrected `double()` function from Step 2.
   so that non-number values passed into the function are handled
-  in some reasonable way.
+  in some reas
+math();onable way.
 */
+
+function math() {
+  var x = 5;
+  function double(num) {
+    if(Number.isInteger(num)) {
+    var x = num * 2;
+    return x;
+  }
+
+  else{
+    console.log('Non-integer value input')
+    return 'NotValid'
+  }
+}
+double(6);
+console.log('The value of x is', x, '-- it should be 5.');
+}
+math();
